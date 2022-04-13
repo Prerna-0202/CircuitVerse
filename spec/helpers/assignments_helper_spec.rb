f601b161296670ccad2b7ba7b13b06424d720f2e
Prerna02 <sharmaprerna0052@gmail.com>
2022-04-13 20:35:02 +0000

    Add specs to assignments_helper_spec.rb

---
+++ spec/helpers/assignments_helper_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe AssignmentsHelper do
+  describe "#deadline_year" do
+    it "return's the assignment.deadline.strftime" do
+      expect(assignment.deadline.strftime).to eq("%Y")
+    end
+  end
+
+  describe "#deadline_month" do
+    it "return's the assignment.deadline.strftime" do
+      expect(assignment.deadline.strftime).to eq("%m")
+    end
+  end
+
+  describe "#deadline_day" do
+    it "return's the assignment.deadline.strftime" do
+      expect(assignment.deadline.strftime).to eq("%-d")
+    end
+  end
+
+  describe "#deadline_hour" do
+    it "return's the assignment.deadline.strftime" do
+      expect(assignment.deadline.strftime).to eq("%H")
+    end
+  end
+
+  describe "#deadline_minute" do
+    it "return's the assignment.deadline.strftime" do
+      expect(assignment.deadline.strftime).to eq("%M")
+    end
+  end
+
+  describe "#deadline_second" do
+    it "return's the assignment.deadline.strftime" do
+      expect(assignment.deadline.strftime).to eq("%S")
+    end
+  end
+end
