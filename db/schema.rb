928f5afc029990ee7c81bd712ae9dd4a7f049ddf
Prerna02 <sharmaprerna0052@gmail.com>
2022-04-25 18:06:30 +0000

    Added Pretty Url for Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_142725) do
+ActiveRecord::Schema.define(version: 2022_04_25_174453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
