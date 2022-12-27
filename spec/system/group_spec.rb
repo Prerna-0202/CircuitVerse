c8c910cd22d9ef1db9d9d6877a080fc2f4711b99
Prerna02 <sharmaprerna0052@gmail.com>
2022-12-27 12:24:07 +0000

    ll

--- spec/system/group_spec.rb
+++ spec/system/group_spec.rb
@@ -36,8 +36,8 @@ describe "Group management", type: :system do
     click_button "+ Add Members"
     execute_script "document.getElementById('addmemberModal').style.display='block'"
     execute_script "document.getElementById('addmemberModal').style.opacity=1"
-    execute_script("document.getElementById('group_email_input').val(@user2.email).trigger('click')")
-    execute_script("document.getElementById('group_email_input').val(" ").trigger('click')")
+    excute_script "document.getElementById('group_email_input').append('<option selected='selected'>'+@user2.email+'</option>')";
+    excute_script "document.getElementById('group_email_input').append('<option selected='selected'>'  '</option>')";
     click_button "Add members"
 
     expect(page).to have_text(
